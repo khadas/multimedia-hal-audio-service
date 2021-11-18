e6d19db09fe728c1df3b0f8ea176e7630671663d
shu.zhang <shu.zhang@amlogic.com>
2021-11-18 01:05:17 -0500

    ACS: subtask, Amazon want to do Audio HAL test	[1/3]
    PD#SWPL-64192
    
    Problem:
    Improve the capture test tool and add options
    
    Solution:
    Improve the capture test tool and add options
    
    Verify:
    ACS-wonka
    
    Change-Id: Ie749f376e85fb9f4575c97f69a880b90dd90e550
    Signed-off-by: shu.zhang <shu.zhang@amlogic.com>

--- src/hal_capture.c
+++ src/hal_capture.c
@@ -24,8 +24,31 @@
 #include "audio_if.h"
 
 int quit_flag = 0;
+int in_device = 0;
 audio_hw_device_t *device;
 struct audio_stream_in *stream;
+
+enum capture_format {
+    FORMAT_PCM_16_BIT = 0,
+    FORMAT_PCM_32_BIT,
+    FORMAT_MAX
+};
+
+enum capture_device {
+    DEVICE_TDM = 0,
+    DEVICE_PDM,
+    DEVICE_MAX
+};
+
+static const char *format_str[] = {
+    "PCM_16_LE",
+    "PCM_32_LE",
+};
+
+static const char *device_str[] = {
+    "TDM",
+    "PDM",
+};
 
 void handler(int sig)
 {
@@ -40,19 +63,22 @@ void capture_read_input_stream()
     char *buffer = NULL;
 
     buffer = (char *)calloc(1, 4*1024 * sizeof(char));
-    FILE *fp = fopen("/data/audio/hal_capture.raw", "a+");
+    FILE *fp = fopen("/data/hal_capture.raw", "r");
+    if (fp != NULL) {
+        fclose(fp);
+        remove("/data/hal_capture.raw");
+    }
+
+    fp = fopen("/data/hal_capture.raw", "a+");
     if (fp) {
         while (!quit_flag) {
             ret = stream->read(stream, buffer, 4*1024);
             read_len += (ret/1024);
-            if (read_len%10 == 0) {
-                printf("data reading:%dk\n", read_len);
-            }
             flen = fwrite((char *)buffer, 1, 4*1024, fp);
             memset(buffer, 0, 4*1024);
         }
 
-        printf("read all data:%d k\n", read_len);
+        printf("Capture data:%d k\n", read_len);
         free(buffer);
         buffer = NULL;
         fclose(fp);
@@ -67,7 +93,7 @@ void capture_open_input_stream(struct audio_config *config)
 
     printf("open input stream...\n");
 
-    ret = device->open_input_stream(device, 0, AUDIO_DEVICE_IN_BUILTIN_MIC, config, &stream, AUDIO_INPUT_FLAG_RAW, NULL, AUDIO_SOURCE_MIC);
+    ret = device->open_input_stream(device, 0, in_device, config, &stream, AUDIO_INPUT_FLAG_RAW, NULL, AUDIO_SOURCE_MIC);
     if (ret) {
         printf("open input stream fail\n");
     } else {
@@ -84,18 +110,122 @@ void capture_close_input_stream()
     return;
 }
 
-int main(int argc, char **argv)
+int capture_parse_options(int argc, char **argv, struct audio_config *config)
+{
+    int i = 0;
+    int c = -1;
+
+    if (argc == 1) {
+        printf("Usage: hal_capture -d <device> -f <format> -c <channel number> -r <sample rate>\n");
+        for (i = 0; i < FORMAT_MAX; i++) {
+            printf("format:\t%d: %s\n", i, format_str[i]);
+        }
+
+        for (i = 0; i < DEVICE_MAX; i++) {
+            printf("device:\t%d: %s\n", i, device_str[i]);
+        }
+        return -1;
+    }
+
+    memset(config, 0, sizeof(struct audio_config));
+    while ((c = getopt(argc, argv, "d:f:c:r:")) != -1) {
+        switch (c) {
+            case 'd':
+                in_device = atoi(optarg);
+                break;
+            case 'f':
+                config->format = atoi(optarg);
+                break;
+            case 'c':
+                config->channel_mask = atoi(optarg);
+                break;
+            case 'r':
+                config->sample_rate = atoi(optarg);
+                break;
+            case '?':
+                fprintf(stderr, "Error in an argument.\n");
+                return -1;
+            default:
+                return -1;
+        }
+    }
+
+    switch (in_device) {
+        case DEVICE_TDM:
+            in_device = AUDIO_DEVICE_IN_WIRED_HEADSET;
+            break;
+        case DEVICE_PDM:
+            in_device = AUDIO_DEVICE_IN_BUILTIN_MIC;
+            break;
+        default:
+            printf("Wrong device, valid format:\n");
+            return -1;
+    }
+
+    switch (config->format) {
+        case FORMAT_PCM_16_BIT:
+            config->format = AUDIO_FORMAT_PCM_16_BIT;
+            break;
+        case FORMAT_PCM_32_BIT:
+            config->format = AUDIO_FORMAT_PCM_32_BIT;
+            break;
+        default:
+            printf("Wrong format, valid format:%d\n", config->format);
+            return -1;
+    }
+
+    switch (config->channel_mask) {
+        case 1:
+            config->channel_mask = AUDIO_CHANNEL_IN_MONO;
+            break;
+        case 2:
+            config->channel_mask = AUDIO_CHANNEL_IN_STEREO;
+            break;
+        default:
+            printf("Wrong channel number, valid range [1~2], channel_mask:%d\n", config->channel_mask);
+            return -1;
+    }
+
+    switch (config->sample_rate) {
+        case 8000:
+        case 11025:
+        case 12000:
+        case 16000:
+        case 17000:
+        case 22050:
+        case 24000:
+        case 32000:
+        case 44100:
+        case 48000:
+        case 96000:
+            break;
+        default:
+            printf("Unsupported (%d) samplerate passed", config->sample_rate);
+            return -1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
 {
     int ret = 0;
     unsigned int support_dev = 0;
     int inited = 0;
     struct audio_config config;
 
+    ret = capture_parse_options(argc, argv, &config);
+    if (ret == -1) {
+        printf("Option error, please check the parameters\n");
+        return ret;
+    }
+
+    /* Register interrupt handling callback */
     signal(SIGINT, handler);
 
     ret = audio_hw_load_interface(&device);
     if (ret) {
-        fprintf(stderr, "%s[%d]: load hw interface error:%d\n", __func__, __LINE__, ret);
+        printf("Load hw interface error:%d\n", ret);
         return ret;
     }
 
@@ -114,11 +244,6 @@ int main(int argc, char **argv)
         printf("device not inited, quit\n");
         goto exit;
     }
-
-    memset(&config, 0, sizeof(config));
-    config.sample_rate = 16000;
-    config.channel_mask = AUDIO_CHANNEL_IN_MONO;
-    config.format = AUDIO_FORMAT_PCM_16_BIT;
 
     capture_open_input_stream(&config);
     capture_read_input_stream(stream);
